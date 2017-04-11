4706be77483f5b629be40be55bf5b153cee09587
Mohamed Babchia <mbabchia@gmail.com>
2017-04-11 12:04:20 +0200

    update DTO

---
+++ src/main/java/com/bmh/coding/model/Customer.java
@@ -0,0 +1,92 @@
+package com.bmh.coding.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+
+import org.hibernate.validator.constraints.Email;
+
+/**
+ * Entity Customer class
+ * 
+ * @author Mohamed
+ *
+ */
+@Entity
+public class Customer {
+
+	/**
+	 * unique Id customer attribute
+	 */
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	/**
+	 * FullName customer attribute
+	 */
+	public String fullName;
+
+	/**
+	 * email customer attribute
+	 */
+	@Email
+	public String email;
+
+	/**
+	 * list of contracts of each customer
+	 * 
+	 * Each Customer may have no, one, or many Contract entities. This is a 1:N
+	 * relationship.
+	 */
+
+	@OneToMany(mappedBy = "customer")
+	@OrderBy("id")
+	public Set<Contract> contracts = new HashSet<>();
+
+	/**
+	 * Constructor with fields attributes
+	 * 
+	 * @param fullName
+	 * @param email
+	 */
+	public Customer(String fullName, String email) {
+
+		this.fullName = fullName;
+		this.email = email;
+	}
+
+	/**
+	 * default constructor
+	 */
+	public Customer() {
+
+	}
+
+	/**
+	 * @return the fullName
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @return the customerId
+	 */
+	public Long getId() {
+		return id;
+	}
+
+}
